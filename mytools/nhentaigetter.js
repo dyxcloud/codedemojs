1f8ee50b16d3b4913090353b281a347be0cda069
dyxLike <dx01@live.cn>
2020-04-08 00:12:08 +0800

    完成解析图片列表功能, 完成打包下载功能

--- mytools/nhentaigetter.js
+++ mytools/nhentaigetter.js
@@ -1,3 +1,19 @@
+// ==UserScript==
+// @name         nhentai图片批量下载
+// @namespace    https://github.com/dyxcloud
+// @version      0.1
+// @description  解析nhentai页面的漫画url, 可以复制所有大图连接, 或者打包下载zip
+// @author       dyxlike
+// @match        https://nhentai.net/g/*
+// @require      https://cdn.jsdelivr.net/npm/jszip@3.2.2/dist/jszip.min.js
+// @require      https://cdn.bootcss.com/FileSaver.js/1.3.8/FileSaver.js
+// @grant        GM_xmlhttpRequest
+// @connect      *
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
 
 /**
  * 获取页面漫画标题
@@ -6,6 +22,18 @@ function getTitle(){
     var h2 = document.querySelector('#info > h2');
     var text = h2.innerHTML;
     return text;
+}
+
+/**
+ * 从文件url来获取文件名
+ * @param {string} url 
+ */
+function getFileName(url){
+    var urlarr = url.split('?');
+    var sp = urlarr[0].split('/');
+    var result = sp[sp.length-1];
+    console.log('解析出一个文件名:'+result);
+    return result;
 }
 
 /**
@@ -21,11 +49,11 @@ function transUrl(url){
 
 function getAllUrl(){
     var imgs = document.querySelectorAll('.gallerythumb > img');
-    var result = '';
+    var result = [];
     for(var img of imgs){
         var src = img.getAttribute('data-src');
         src = transUrl(src);
-        result +=  (src + '\r\n');
+        result.push(src);
     }
     console.log(result);
     return result;
@@ -50,28 +78,95 @@ function copyText(text){
 /**
  * 入口函数
  */
-function main(self){
-    console.log(getTitle());
-    copyText(getAllUrl());
+function mainCopy(self){
+    var result = getAllUrl();
+    var line = '';
+    for(var u of result){
+        line +=  (u + '\r\n');
+    }
+    copyText(line);
     self.innerText = '复制完成';
-    var event = event || window.event;
-    event.preventDefault();
+    // var event = event || window.event;
+    // event.preventDefault();
 }
-
-var topmenus = document.querySelectorAll('.desktop');
-var info = topmenus[topmenus.length-1];
-var a = info.querySelector('a');
-a.innerText = '点击复制图片';
-a.onclick = function(){ main(this); }
 
 
 /**
- * 思路:
- * 获取标题和url
- * 跳转到i.nhentai.net
- * fetch获取数据, 并打包, 下载
+ * 返回blob数组
+ * @param {Array<string>} urls 
  */
+function fetchBlobs(urls){
+    console.log("fetchBlobs获取到个数:"+urls.length);
+    return Promise.all(urls.map(GMFetch));
+}
+
+/**
+ * 返回blob
+ * @param {string} url 
+ */
+function GMFetch(url){
+    return new Promise((resolve)=>{
+        console.log('开始请求:'+url);
+        GM_xmlhttpRequest({
+            url : url,
+            responseType: 'blob',
+            onload : function(re){
+                var blob = re.response; //TODO 此处必须先获取到blob对象再赋值
+                blob.name = getFileName(url);
+                console.log('获取到请求响应'+blob);
+                console.log('获取到请求响应'+blob.size);
+                console.log('获取到请求响应'+blob.name);
+                resolve(blob);
+            },
+            onerror:function(){
+                console.log('请求失败:'+url);
+            }
+        });
+    }).then((blob)=>{return blob});
+}
+
+/**
+ * blob数组打压缩包
+ * @param {Array<Blob>} blobs 
+ */
+function pack(blobs) {
+    console.log('获取到blob个数:'+blobs.length);
+    const zip = new JSZip();
+    blobs.forEach((blob) => zip.file(blob.name, blob));
+    return zip.generateAsync({type : "blob"});
+}
+
+
+function mainDownloadZip(self){
+    var urls = getAllUrl();
+    fetchBlobs(urls).then(pack).then((zipblob) => {
+        saveAs(zipblob, getTitle() + ".zip");
+    });
+    
+    self.innerText = '下载完成';
+    // var event = event || window.event;
+    // event.preventDefault();
+}
+
+
+
+
+
+//生成复制按钮
+var topmenus = document.querySelectorAll('.desktop');
+var info = topmenus[topmenus.length-1];
+var a = info.querySelector('a');
+a.innerText = '点击复制图片链接';
+a.onclick = function(){ mainCopy(this);return false; }
+//生成下载按钮
+var cloneN = info.cloneNode(true);
+a = cloneN.querySelector('a');
+a.innerText = '点击打包下载';
+a.onclick = function(){ mainDownloadZip(this);return false; }
+info.parentNode.appendChild(cloneN);
 
 
 //https://nhentai.net/g/307036/
 
+
+})();
