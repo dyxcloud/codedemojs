63cb3d9c92437d9a28b74d046fe79a4913ac54bb
dyxLike <dx01@live.cn>
2020-07-27 13:46:47 +0800

    坦克世界击杀环排序

---
+++ mytools/wotsort.js
@@ -0,0 +1,68 @@
+// ==UserScript==
+// @name         坦克世界击杀环标伤数据排序 WOT world of tanks
+// @name:en      World of Tanks Kill ring sort
+// @namespace    https://github.com/dyxcloud
+// @version      0.0.2
+// @description  将毛服击杀环的数据进行排序
+// @description:en  Sort the data of Russian service kill ring
+// @author       dyxlike
+// @match        https://lebwa.tv/lebwa-team/marks-of-excellence
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+
+/**
+ * 排序
+ * @param {string} type 排序字段
+ * @param {string} order asc或其他
+ */
+function sort(type, order) {
+    let table = document.querySelector("#app > div.section-content > div > div:nth-child(3) > div > div > div > table > tbody");
+    let trArr = table.children;
+    let intType;
+    if (type == "65") {
+        intType = 4;
+    } else if (type == "85") {
+        intType = 5;
+    } else if (type == "95") {
+        intType = 6;
+    } else if (type == "100") {
+        intType = 7;
+    }
+    let temp
+    if (order == "asc") {
+        for (let i = 0; i < trArr.length - 1; i++) {
+            for (let j = 0; j < trArr.length - i - 1; j++) {
+                if ((trArr[j].children[intType].innerHTML - 0) > (trArr[j + 1].children[intType].innerHTML - 0)) {
+                    temp = trArr[j].innerHTML;
+                    trArr[j].innerHTML = trArr[j + 1].innerHTML;
+                    trArr[j + 1].innerHTML = temp;
+                }
+            }
+        }
+    } else {
+        for (let i = 0; i < trArr.length - 1; i++) {
+            for (let j = 0; j < trArr.length - i - 1; j++) {
+                if ((trArr[j].children[intType].innerHTML - 0) < (trArr[j + 1].children[intType].innerHTML - 0)) {
+                    temp = trArr[j].innerHTML;
+                    trArr[j].innerHTML = trArr[j + 1].innerHTML;
+                    trArr[j + 1].innerHTML = temp;
+                }
+            }
+        }
+    }
+}
+
+let e65 = document.querySelector("#app > div.section-content > div > div:nth-child(3) > div > div > div > table > thead > tr > th:nth-child(5)");
+let e85 = document.querySelector("#app > div.section-content > div > div:nth-child(3) > div > div > div > table > thead > tr > th:nth-child(6)");
+let e95 = document.querySelector("#app > div.section-content > div > div:nth-child(3) > div > div > div > table > thead > tr > th:nth-child(7)");
+let e100 = document.querySelector("#app > div.section-content > div > div:nth-child(3) > div > div > div > table > thead > tr > th:nth-child(8)");
+
+e65.addEventListener('click', function(){sort("65","");});
+e85.addEventListener('click', function(){sort("85","");});
+e95.addEventListener('click', function(){sort("95","");});
+e100.addEventListener('click', function(){sort("100","");});
+
+})();
