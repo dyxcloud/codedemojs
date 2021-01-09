fd2cf6158eddc9749d38ef3f0561dfc71016cb21
dyxLike <dx01@live.cn>
2021-01-09 19:06:20 +0800

    优化代码,表格添加图标列

--- src/js/apitest.js
+++ src/js/apitest.js
@@ -1,6 +1,7 @@
-let url = "https://t.nhentai.net/galleries/1599898/1t.jpg";
-url = url.replace('https://t.nhentai.net', 'https://i.nhentai.net');
-url = url.replace('t.jpg', '.jpg', 1);
-url = url.replace('t.jpg', '.jpg');
-
-console.log(url);
+fetch('https://tanks.gg/api/list')
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(myJson) {
+        console.log(myJson);
+    });
--- src/mytools/wot-sort.js
+++ src/mytools/wot-sort.js
@@ -12,34 +12,47 @@
 (function () {
     'use strict';
 
-    //https://tanks.gg/api/list
+    /**
+     * 获取坦克表格行tr列表
+     * @returns {HTMLCollection}
+     */
+    function getRowList() {
+        let table = document.querySelector("#app > div.section-content > div > div:nth-child(3) > div > div > div > table > tbody");
+        return table.children;
+    }
 
     /**
-     * 排序
+     * 降序排序
      * @param {string} type 排序字段
      */
     function sort(type) {
-        let intType;
-        if (type === "65") {
-            intType = 4;
-        } else if (type === "85") {
-            intType = 5;
-        } else if (type === "95") {
-            intType = 6;
-        } else if (type === "100") {
-            intType = 7;
-        } else {
-            return;
+        let markClass; //标伤单元格的class样式
+        switch (type) {
+            case "65":
+                markClass = ".value-0";
+                break;
+            case "85":
+                markClass = ".value-1";
+                break;
+            case "95":
+                markClass = ".value-2";
+                break;
+            case "100":
+                markClass = ".value-3";
+                break;
+            default:
+                return;
         }
 
-        let table = document.querySelector("#app > div.section-content > div > div:nth-child(3) > div > div > div > table > tbody");
-        let trArr = table.children;
+        let trArr = getRowList();
         let length = trArr.length;
+        console.log(trArr);
+        console.log(length);
         for (let i = 1; i < length; i++) {
             let tmpHtml = trArr[i].innerHTML;
-            let tmpScore = getScore(trArr[i], intType);
+            let tmpScore = getScore(trArr[i], markClass);
             let j = i - 1;
-            while (j >= 0 && tmpScore > getScore(trArr[j], intType)) {
+            while (j >= 0 && tmpScore > getScore(trArr[j], markClass)) {
                 trArr[j + 1].innerHTML = trArr[j].innerHTML;
                 j--;
             }
@@ -47,31 +60,88 @@
         }
     }
 
-    const reg = /\d+/;
-    function getScore(rowTag,intType){
-        let param = rowTag.children[intType].innerHTML;
-        if(!reg.test(param)){
+
+    /**
+     * 获取标伤数值
+     * @param rowTag
+     * @param markClass 标伤单元格的class样式
+     * @returns {number}
+     */
+    function getScore(rowTag, markClass) {
+        const reg = /\d+/;
+        let param = rowTag.querySelector(markClass).innerHTML;
+        if (!reg.test(param)) {
             return 0;
         }
         return param - 0;
     }
 
-    let e65 = document.querySelector("#app > div.section-content > div > div:nth-child(3) > div > div > div > table > thead > tr > th:nth-child(5)");
-    let e85 = document.querySelector("#app > div.section-content > div > div:nth-child(3) > div > div > div > table > thead > tr > th:nth-child(6)");
-    let e95 = document.querySelector("#app > div.section-content > div > div:nth-child(3) > div > div > div > table > thead > tr > th:nth-child(7)");
-    let e100 = document.querySelector("#app > div.section-content > div > div:nth-child(3) > div > div > div > table > thead > tr > th:nth-child(8)");
 
-    e65.addEventListener('click', function () {
-        sort("65");
-    });
-    e85.addEventListener('click', function () {
-        sort("85");
-    });
-    e95.addEventListener('click', function () {
-        sort("95");
-    });
-    e100.addEventListener('click', function () {
-        sort("100");
-    });
+    /**
+     * 从tasks.gg获取坦克列表
+     * @returns {Promise<JSON>}
+     */
+    function getTaskListJson() {
+        const TASK_LIST_URL = "https://tanks.gg/api/list";
+        return fetch(TASK_LIST_URL)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (myJson) {
+                return myJson;
+            });
+    }
+
+    /**
+     * TODO 添加金币车图标
+     */
+    function addGoldTag() {
+        //处理表头 表尾
+        let tHead = document.querySelector("#app > div.section-content > div > div:nth-child(3) > div > div > div > table > thead > tr");
+        let thElement = document.createElement('th');
+        thElement.className = "text-center";
+        thElement.innerHTML = "Gold";
+        tHead.appendChild(thElement);
+        let tFoot = document.querySelector("#app > div.section-content > div > div:nth-child(3) > div > div > div > table > tfoot > tr");
+        let thFootElement = document.createElement('th');
+        thFootElement.className = "text-center";
+        thFootElement.innerHTML = "Gold";
+        tFoot.appendChild(thFootElement);
+        //处理表体
+        let trArr = getRowList();
+        let length = trArr.length;
+        for(let i=0;i<length;i++){
+            let tr = trArr[i];
+            let td_element = document.createElement('td');
+            td_element.innerHTML = "<img src='https://tanks.gg/img/icons/gold.png'  alt />";
+            tr.appendChild(td_element);
+            console.log("added");
+        }
+    }
+
+    function main() {
+        let e65 = document.querySelector("#app > div.section-content > div > div:nth-child(3) > div > div > div > table > thead > tr > th:nth-child(5)");
+        let e85 = document.querySelector("#app > div.section-content > div > div:nth-child(3) > div > div > div > table > thead > tr > th:nth-child(6)");
+        let e95 = document.querySelector("#app > div.section-content > div > div:nth-child(3) > div > div > div > table > thead > tr > th:nth-child(7)");
+        let e100 = document.querySelector("#app > div.section-content > div > div:nth-child(3) > div > div > div > table > thead > tr > th:nth-child(8)");
+        e65.addEventListener('click', function () {
+            sort("65");
+        });
+        e85.addEventListener('click', function () {
+            sort("85");
+        });
+        e95.addEventListener('click', function () {
+            sort("95");
+        });
+        e100.addEventListener('click', function () {
+            sort("100");
+        });
+
+        setTimeout(function () {
+            addGoldTag();
+        },3000);
+    }
+
+    main();
 
 })();
