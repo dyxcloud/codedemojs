ed169cf8a4c0bc29062ca31bd5eb01bfc70e7c50
dyxLike <dx01@live.cn>
2023-02-25 20:56:36 +0800

    修复适配路径

--- src/mytools/bilibili-bgm-filter.js
+++ src/mytools/bilibili-bgm-filter.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         B站订阅页面自动筛选"在看"番剧和电影
 // @namespace    https://github.com/dyxcloud
-// @version      0.1
+// @version      0.2
 // @description  订阅页面的筛选自动选择"在看", 在"追番"和"追剧"两个页面生效
 // @author       dyxLike
-// @match        https://space.bilibili.com/*/bangumi
-// @match        https://space.bilibili.com/*/cinema
+// @match        https://space.bilibili.com/*/bangumi*
+// @match        https://space.bilibili.com/*/cinema*
 // @grant        none
 // ==/UserScript==
 
