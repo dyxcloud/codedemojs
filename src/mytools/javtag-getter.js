ecdc2e8a2e8b952f9497b028c1803b818128f7c5
dyxLike <dx01@live.cn>
2025-03-16 01:58:24 +0800

    javtag: 获取发行商和日期

--- src/mytools/javtag-getter.js
+++ src/mytools/javtag-getter.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         jav tag copy
 // @namespace    https://github.com/dyxcloud
-// @version      0.5.7.1
+// @version      0.5.8.0
 // @description  获取页面的tag,返回带tag的文件名 "番号标题 #tag1 #tag2.."
 // @author       dyxlike
 // @include     *://*javlibrary.com/*
@@ -16,162 +16,204 @@
 (function() {
     'use strict';
 
+    // 定义黑名单标签列表
+    const blackTags = ['单体作品', '單體作品', '数位马赛克', '數位馬賽克'];
 
+    function javLibGetter(){
+        let result = '';
+        // 获取标题
+        let title = document.querySelector('#video_title > h3').innerText;
+        result+=title;
 
-function javLibGetter(){
-    let result = '';
-    //获取标题
-    let title = document.querySelector('#video_title > h3').innerText;
-    console.log("title="+title);
-    result+=title;
-
-    //获取star
-    let starAs = document.querySelectorAll('span.star > a');
-    if (starAs.length > 0) {
-        result += " ";
-        for(let a of starAs){
-            console.log("star="+a.text);
-            result+=("#"+a.text);
+        // 获取star
+        let starAs = document.querySelectorAll('span.star > a');
+        let stars = Array.from(starAs)
+            .map(a => a.text)
+            .join("#");
+        if (stars !== "") {
+            result += " #" + stars;
         }
+
+        // 获取发行商
+        let label = document.querySelector('#video_label .text a').innerText;
+        // 获取发行日期
+        let releaseDate = document.querySelector('#video_date .text').innerText;
+        if (label !== "" || releaseDate !== "") {
+            result += " ";
+            if (label !== "") {
+                result += ("#" + label);
+            }
+            if (releaseDate !== "") {
+                result += ("#" + releaseDate);
+            }
+        }
+
+        // 获取tag
+        let tagAs = document.querySelectorAll('#video_genres span.genre > a');
+        let validTags = Array.from(tagAs)
+            .map(a => a.text)
+            .filter(tag => !blackTags.includes(tag))
+            .join("#");
+        if (validTags !== "") {
+            result += " #" + validTags;
+        }
+        return result;
     }
 
-    //获取tag
-    let tagAs = document.querySelectorAll('#video_genres span.genre > a');
-    if(tagAs.length>0){
-        result += " ";
-        for(let a of tagAs){
-            console.log("tag="+a.text);
-            result+=("#"+a.text);
-        }
-    }
-    return result;
-}
+    function javLib(){
+        let button = document.createElement('a');
+        button.innerText='点击复制tag文件名';
+        button.href="#";
+        let video_info = document.querySelector('#video_info');
+        video_info.append(button);
 
-function javLib(){
-    let button = document.createElement('a');
-    button.innerText='点击复制tag文件名';
-    button.href="#";
-    let video_info = document.querySelector('#video_info');
-    video_info.append(button);
-
-    button.onclick = function () {
-        let result = javLibGetter();
-        GM_setClipboard(result);
-    };
-}
-
-function javBusGetter(){
-    let result = '';
-    //获取标题
-    let title = document.querySelector('body > div.container > h3').innerText;
-    console.log("title="+title);
-    result+=title;
-
-    //获取star
-    let starAs = document.querySelectorAll('span.genre > a[href*="/star/"]');
-    if (starAs.length > 0) {
-        result += " ";
-        for(let a of starAs){
-            console.log("star="+a.text);
-            result+=("#"+a.text);
-        }
+        button.onclick = function () {
+            let result = javLibGetter();
+            GM_setClipboard(result);
+        };
     }
 
-    //获取tag
-    let tagAs = document.querySelectorAll('span.genre a[href*="/genre/"]');
-    if(tagAs.length>0){
-        result += " ";
-        for(let a of tagAs){
-            console.log("tag="+a.text);
-            result+=("#"+a.text);
+    function javBusGetter(){
+        let result = '';
+        // 获取标题
+        let title = document.querySelector('body > div.container > h3').innerText;
+        console.log("title="+title);
+        result+=title;
+
+        // 获取star
+        let starAs = document.querySelectorAll('span.genre > a[href*="/star/"]');
+        let stars = Array.from(starAs)
+            .map(a => a.text)
+            .join("#");
+        if (stars !== "") {
+            result += " #" + stars;
         }
-    }
-    return result;
-}
 
-function javBus(){
-    let button = document.createElement('a');
-    button.innerText='点击复制tag文件名';
-    button.href="#";
-    let video_info = document.querySelector('body > div.container > div.row.movie > div.col-md-3.info');
-    video_info.append(button);
-
-    button.onclick = function () {
-        let result = javBusGetter();
-        GM_setClipboard(result);
-    };
-}
-
-function avSoxGetter(){
-    let result = '';
-    //获取标题
-    let title = document.querySelector('body > div.container > h3').innerText;
-    console.log("title="+title);
-    result+=title;
-
-    //获取star
-    let starAs = document.querySelectorAll('#avatar-waterfall > a');
-    if (starAs.length > 0) {
-        result += " ";
-        for(let a of starAs){
-            let span = a.children[1];
-            console.log("star="+span.innerText);
-            result+=("#"+span.innerText);
+        // 获取发行商
+        let labelAs = document.querySelectorAll('#video_info a[href*="/label/"]');
+        let label = labelAs.length > 0 ? labelAs[0].innerText : "";
+        // 获取发行日期
+        let releaseDateE = document.querySelectorAll('#video_info > p:nth-child(2)');
+        let releaseDate = releaseDateE.length > 0 ? releaseDateE[0].innerText.replace("發行日期:", "").trim() : "";
+        if (label !== "" || releaseDate !== "") {
+            result += " ";
+            if (label !== "") {
+                result += ("#" + label);
+            }
+            if (releaseDate !== "") {
+                result += ("#" + releaseDate);
+            }
         }
-    }
 
-    //获取tag
-    let tagAs = document.querySelectorAll('span.genre a[href*="/genre/"]');
-    if(tagAs.length>0){
-        result += " ";
-        for(let a of tagAs){
-            console.log("tag="+a.text);
-            result+=("#"+a.text);
+        // 获取tag
+        let tagAs = document.querySelectorAll('span.genre a[href*="/genre/"]');
+        let validTags = Array.from(tagAs)
+            .map(a => a.text)
+            .filter(tag => !blackTags.includes(tag))
+            .join("#");
+        if (validTags !== "") {
+            result += " #" + validTags;
         }
+        return result;
     }
-    result+=("#无码");
-    return result;
-}
 
-function avSox(){
-    let button = document.createElement('a');
-    button.innerText='点击复制tag文件名';
-    button.href="#";
-    let video_info = document.querySelector("div.col-md-3.info");
-    video_info.append(button);
+    function javBus(){
+        let button = document.createElement('a');
+        button.innerText='点击复制tag文件名';
+        button.href="#";
+        let video_info = document.querySelector('body > div.container > div.row.movie > div.col-md-3.info');
+        video_info.append(button);
 
-    button.onclick = function () {
-        let result = avSoxGetter();
-        GM_setClipboard(result);
-    };
-}
+        button.onclick = function () {
+            let result = javBusGetter();
+            GM_setClipboard(result);
+        };
+    }
 
+    function avSoxGetter(){
+        let result = '';
+        // 获取标题
+        let title = document.querySelector('body > div.container > h3').innerText;
+        console.log("title="+title);
+        result+=title;
+
+        // 获取star
+        let starAs = document.querySelectorAll('#avatar-waterfall > a');
+        let stars = Array.from(starAs)
+            .map(a => a.children[1].innerText)
+            .join("#");
+        if (stars !== "") {
+            result += " #" + stars;
+        }
+
+        // 获取tag
+        let tagAs = document.querySelectorAll('span.genre a[href*="/genre/"]');
+        let validTags = Array.from(tagAs)
+            .map(a => a.text)
+            .filter(tag => !blackTags.includes(tag))
+            .join("#");
+        if (validTags !== "") {
+            result += " #" + validTags;
+        }
+        result+=("#无码");
+        return result;
+    }
+
+    function avSox(){
+        let button = document.createElement('a');
+        button.innerText='点击复制tag文件名';
+        button.href="#";
+        let video_info = document.querySelector("div.col-md-3.info");
+        video_info.append(button);
+
+        button.onclick = function () {
+            let result = avSoxGetter();
+            GM_setClipboard(result);
+        };
+    }
 
     function javDbGetter(){
         let result = '';
-        //获取标题
+        // 获取标题
         let title = document.querySelector('.title.is-4').innerText;
         console.log("title="+title);
         result+=title;
 
-        //获取star
+        // 获取star
         let starAs = document.querySelectorAll('nav.panel.movie-panel-info a[href*="/actors/"]');
-        if (starAs.length > 0) {
+        let stars = Array.from(starAs)
+            .map(a => a.text)
+            .join("#");
+        if (stars !== "") {
+            result += " #" + stars;
+        }
+
+
+        // 获取发行商
+        let labelAs = document.querySelectorAll('.movie-panel-info a[href*="/publishers/"]');
+        let label = labelAs.length > 0 ? labelAs[0].innerText : "";
+        // 获取发行日期
+        let releaseDateE = document.querySelectorAll('.movie-panel-info > div:nth-child(2)');
+        let releaseDate = releaseDateE.length > 0 ? releaseDateE[0].innerText.replace("日期:", "").trim() : "";
+        if (label !== "" || releaseDate !== "") {
             result += " ";
-            for(let a of starAs){
-                console.log("star="+a.text);
-                result+=("#"+a.text);
+            if (label !== "") {
+                result += ("#" + label);
+            }
+            if (releaseDate !== "") {
+                result += ("#" + releaseDate);
             }
         }
 
-        //获取tag
+
+        // 获取tag
         let tagAs = document.querySelectorAll('nav.panel.movie-panel-info a[href*="/tags"]');
-        if(tagAs.length>0){
-            result += " ";
-            for(let a of tagAs){
-                console.log("tag="+a.text);
-                result+=("#"+a.text);
-            }
+        let validTags = Array.from(tagAs)
+            .map(a => a.text)
+            .filter(tag => !blackTags.includes(tag))
+            .join("#");
+        if (validTags !== "") {
+            result += " #" + validTags;
         }
         return result;
     }
@@ -196,20 +238,20 @@ function avSox(){
             GM_setClipboard(result+'#无码');
         };
     }
-    
-function main(){
-    let title = document.title;
-    if ((/JAVLib/g).test(title)) {
-        javLib();
-    } else if ((/JavBus/g).test(title)) {
-        javBus();
-    } else if ((/AVSOX/g).test(title)) {
-        avSox();
-    } else if ((/JavDB/g).test(title)) {
-        javDb();
-    }
-}
 
-main();
+    function main(){
+        let title = document.title;
+        if ((/JAVLib/g).test(title)) {
+            javLib();
+        } else if ((/JavBus/g).test(title)) {
+            javBus();
+        } else if ((/AVSOX/g).test(title)) {
+            avSox();
+        } else if ((/JavDB/g).test(title)) {
+            javDb();
+        }
+    }
+
+    main();
 
 })();
